596ef1ad76061769532c3712d1d077d5bd94e5a2
AbdElOuahab Radja <abdelouahabradja@gmail.com>
2023-06-14 19:03:00 +0100

    setup api commit

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import cv2
 import numpy as np
 import tensorflow as tf
 import mysql.connector
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 
@@ -29,15 +31,24 @@ xray_class_names = {
 
 model = tf.keras.models.load_model("mobilenet_model.h5")
 
+@app.route('/', methods=['GET'])
+def welcome():
+    response = "<h1 style='color:#04aa6d'>welcome to doctor ai collab web api</h1>"
+    return response
+
+@app.route('/info', methods=['GET'])
+def modelinfo():
+    response = "<h1 style='color:#04aa6d'>Supported Classes by Our API:</h1>"
+    response += "<pre>"
+    for val in xray_class_names.values():
+        response += val + "\n"
+    return response
 
 @app.route('/predict', methods=['GET'])
 def predict():
     image_id = request.args.get('id')
-
     if image_id is None or not image_id.isdigit() or int(image_id) <= 0:
         return "Bad Request!"
-
-    # return image_id
 
     # Connect to the MySQL database
     try:
@@ -52,33 +63,34 @@ def predict():
 
     cursor = db.cursor()
 
-    # Fetch the image from the database based on the ID
+    # Fetching the image from the database
     query = "SELECT image_binary FROM blog_images WHERE id = %s"
     cursor.execute(query, (image_id,))
     result = cursor.fetchone()
 
-    # Check if the image exists
     if result is None:
         return jsonify({'error': 'Image not found'}), 404
 
-    # Convert the binary data to numpy array
+    # binary ---> numpy array
     image_data = np.frombuffer(result[0], np.uint8)
 
-    # Decode the image
+    # Decode the image to cv2 obj
     image = cv2.imdecode(image_data, cv2.IMREAD_COLOR)
 
-    # Preprocess the image
+    # Resizing the image
     resized_image = cv2.resize(image, IMAGE_SHAPE)
     processed_image = np.expand_dims(resized_image, axis=0)
 
-    # Make predictions using the loaded model
+    # generate predictions
     predictions = model.predict(processed_image)
 
-    # Convert the predictions to a dictionary with class names and their ratios
-    results = {xray_class_names[class_index]: float(predictions[0][class_index]) for class_index in range(len(xray_class_names))}
+    # results = {xray_class_names[class_index]: float(predictions[0][class_index]) for class_index in range(len(xray_class_names))}
+    results = {
+        xray_class_names[class_index]: "{:.2f}".format(float(predictions[0][class_index]) * 100)
+        for class_index in range(len(xray_class_names))
+    }
 
-    # Return the results as a JSON response
     return jsonify(results)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='api.doctoraicollab.test', port=5000)
