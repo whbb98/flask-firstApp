e3df5f59ca0543189076f4d53e810d7ebe96d379
Abdelouahab Radja <abdelouahabradja@gmail.com>
2023-12-16 11:53:07 +0100

    updated the script to handle multiple models

--- app.py
+++ app.py
@@ -2,15 +2,16 @@ from flask import Flask, request, jsonify
 import cv2
 import numpy as np
 import tensorflow as tf
-import mysql.connector
 from flask_cors import CORS
+import base64
 
 app = Flask(__name__)
-CORS(app, origins='http://doctoraicollab.test')
+APP_URL = 'localhost'
+CORS(app, origins=APP_URL)
 
-# Define the necessary constants
 IMAGE_SHAPE = (224, 224)
-
+model_v1 = tf.keras.models.load_model("mobilenet_model.h5")
+# model_v2 = tf.keras.models.load_model("")
 xray_class_names = {
     0: 'Atelectasis',
     1: 'Cardiomegaly',
@@ -29,68 +30,65 @@ xray_class_names = {
 }
 
 
-model = tf.keras.models.load_model("mobilenet_model.h5")
+def decode_image64(image64):
+    try:
+        if image64 is None:
+            raise (Exception('Image64 is Null'))
+        prefix, image64 = image64.split(',', 1)
+        image_type = prefix.split(';')[0].split(':')[1]
+        image64 += '=' * ((4 - len(image64) % 4) % 4)
+        image_data = base64.b64decode(image64)
+        return image_data
+    except Exception as e:
+        return jsonify({'error': 'decoding base64 image failed'}), 400
+
 
 @app.route('/', methods=['GET'])
 def welcome():
     response = "<h1 style='color:#04aa6d'>welcome to doctor ai collab web api</h1>"
     return response
 
-@app.route('/info', methods=['GET'])
-def modelinfo():
-    response = "<h1 style='color:#04aa6d'>Supported Classes by Our API:</h1>"
-    response += "<pre>"
+
+@app.route('/model_v0', methods=['GET'])
+def model_v0_info():
+    return jsonify({'greeting': 'welcome to /model_v0/predict!'})
+
+
+@app.route('/model_v1', methods=['GET'])
+def model_v1_info():
+    response = "<h1 style='color:#04aa6d'>Supported classes by model-v1:</h1>"
+    response += "<ul>"
     for val in xray_class_names.values():
-        response += val + "\n"
+        response += f"<li>{val}</li>"
+    response += "</ul>"
     return response
 
-@app.route('/predict', methods=['GET'])
-def predict():
-    image_id = request.args.get('id')
-    if image_id is None or not image_id.isdigit() or int(image_id) <= 0:
-        return "Bad Request!"
 
-    # Connect to the MySQL database
+@app.route('/model_v1/predict', methods=['POST'])
+def model_v1_predict():
     try:
-        db = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="",
-            database="doctoraicollab"
-        )
-    except:
-        return "Couldn't Connect to Database!"
+        data = request.get_json()
+        image64 = data.get('image64')
+        if image64 is None:
+            return jsonify({'error': 'Base64 image not provided'}), 400
+    except Exception as e:
+        return jsonify({'error': 'Bad request format'}), 400
 
-    cursor = db.cursor()
+    try:
+        image_data = decode_image64(image64)
+        image = cv2.imdecode(np.frombuffer(image_data, np.uint8), cv2.IMREAD_COLOR)
+    except Exception as e:
+        return jsonify({'error': 'Image Processing Failed!'}), 400
 
-    # Fetching the image from the database
-    query = "SELECT image_binary FROM blog_images WHERE id = %s"
-    cursor.execute(query, (image_id,))
-    result = cursor.fetchone()
-
-    if result is None:
-        return jsonify({'error': 'Image not found'}), 404
-
-    # binary ---> numpy array
-    image_data = np.frombuffer(result[0], np.uint8)
-
-    # Decode the image to cv2 obj
-    image = cv2.imdecode(image_data, cv2.IMREAD_COLOR)
-
-    # Resizing the image
     resized_image = cv2.resize(image, IMAGE_SHAPE)
     processed_image = np.expand_dims(resized_image, axis=0)
-
-    # generate predictions
-    predictions = model.predict(processed_image)
-
-    # results = {xray_class_names[class_index]: float(predictions[0][class_index]) for class_index in range(len(xray_class_names))}
+    predictions = model_v1.predict(processed_image)
     results = {
         xray_class_names[class_index]: "{:.2f}".format(float(predictions[0][class_index]) * 100)
         for class_index in range(len(xray_class_names))
     }
-
     return jsonify(results)
 
+
 if __name__ == '__main__':
-    app.run(host='api.doctoraicollab.test', port=5000)
+    app.run(host=APP_URL, port=5000)
